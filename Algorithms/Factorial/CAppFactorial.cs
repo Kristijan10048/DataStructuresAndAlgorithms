5e92db0bbab2f443c9e3fcccd29cad533b28cdff
kristijan10048 <kristijan.petkov@gmail.com>
2018-06-21 22:53:10 +0200

    initial commit

---
+++ Algorithms/Factorial/CAppFactorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class CAppFactorial
+    {
+        private static int Fact(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else return n * Fact(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(string.Format("i = {0}, fact = {1}", i, Fact(i)));
+            Console.ReadKey();
+        }
+    }
+}
