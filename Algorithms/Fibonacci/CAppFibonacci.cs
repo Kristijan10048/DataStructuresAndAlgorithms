bcd0bada76959ccd56364e1d0c31894a6dac3f65
kristijan10048 <kristijan.petkov@gmail.com>
2018-06-21 22:25:27 +0200

    initial commit

---
+++ Algorithms/Fibonacci/CAppFibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class CAppFibonacci
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n"></param>
+        private static int F(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+            {
+              int res = F(n - 1) + F(n - 2);
+                Console.WriteLine("N = "+ n + "Res = " + res);
+                return res;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<=10; i++)
+            {
+                Console.Write(F(i) + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
