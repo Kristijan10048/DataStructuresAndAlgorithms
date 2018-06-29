6c79bf0ab8ccd944753dec66bc18c69ed0454864
kristijan10048 <kristijan.petkov@gmail.com>
2018-06-29 16:31:29 +0200

    Euclid GCD algo

---
+++ Algorithms/07.EuclidGCD/CAppGcd.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.EuclidGCD
+{
+    class CAppGcd
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns>GCD</returns>
+        private static int Gcd(int p, int q)
+        {
+            if (p == 0)
+                return q;
+            else if (q == 0)
+                return p;
+            else
+            {
+                int r = p % q;
+                Console.WriteLine("p = {0}, q = {1}, r = {2}", p, q, r);
+                return Gcd(q, r);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int p = 270, q = 192;
+            int tmpGcd = Gcd(p, q);
+            Console.WriteLine("Gcd of p = {0} and q = {1} is {2}", p, q, tmpGcd);
+
+            p = 1111111;
+            q = 1234567;
+            tmpGcd = Gcd(p, q);
+            Console.WriteLine("Gcd of p = {0} and q = {1} is {2}", p, q, tmpGcd);
+
+            Console.ReadKey();
+        }
+    }
+}
