35a4349b5ff54ca9ed981bd1fb8f644254708dbb
kristijan10048 <kristijan.petkov@gmail.com>
2018-07-06 19:09:11 +0200

    Add tests poroject

---
+++ Algorithms/Tests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IComparable a = 10, b = 4;
+            Console.WriteLine("{0}.CompareTo({1}) = {2}", a, b, a.CompareTo(b));
+
+            a = 3; b = 11;
+            Console.WriteLine("{0}.CompareTo({1}) = {2}", a, b, a.CompareTo(b));
+
+            a = 3; b = 3;
+            Console.WriteLine("{0}.CompareTo({1}) = {2}", a, b, a.CompareTo(b));
+
+            Console.ReadKey();
+        }
+    }
+}
