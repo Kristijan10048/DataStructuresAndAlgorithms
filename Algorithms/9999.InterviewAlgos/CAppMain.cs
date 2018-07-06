241becd9dcc772006ecfc852bb03f50d12a765b7
kristijan10048 <kristijan.petkov@gmail.com>
2018-07-06 19:10:04 +0200

    Initial commit of interview algos

---
+++ Algorithms/9999.InterviewAlgos/CAppMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _9999.InterviewAlgos.Ch1_ArraysAndStrings;
+
+namespace _9999.InterviewAlgos
+{
+    class CAppArraysAndStrings
+    {
+        static void Main(string[] args)
+        {
+            //Write code to remove duplicates from an unsorted linked list
+            CAppDeleteDuplInLinkedList.TestIt();
+
+            // Assume you have a method isSubstring which checks if one word is a isSubstring of another.
+            // Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only
+            // one call to isSubstring(e.g., "waterbottle" is a rotation of "erbottlewat").
+
+        }
+    }
+}
